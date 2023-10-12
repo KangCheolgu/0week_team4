dae3aefbb7b926c20db671df640524c501752b70
강철구 <opopqwqw123@gmail.com>
2023-10-12 14:53:23 +0900

    글작성과 메인페이지 연결

--- app.py
+++ app.py
@@ -43,6 +43,8 @@ def mainpage():
     except jwt.exceptions.DecodeError:
         return redirect("http://localhost:5000/")
 
+
+# 로그인 회원가입 관련
 @app.route('/login', methods=['POST'])
 def login():
    
@@ -107,15 +109,20 @@ def idcheck():
         return jsonify({'result': 'success'})
     else :
         return jsonify({'result': 'fail'})   
-
-@app.route('/mealmenu', methods=['POST'])
-def menuScraping():
+##########################################################
 
 
+#학식 메뉴 관련
+# @app.route('/mealmenu', methods=['POST'])
+# def menuScraping():
 
-    return menu
-  
 
+
+#     return menu
+##########################################################
+
+
+# 메인페이지 전체 리스트 관련
 @app.route('/api/list', methods=['GET'])
 def show_rests():
     sortMode = request.args.get('sortMode', 'like')
@@ -128,8 +135,121 @@ def show_rests():
         return jsonify({'result': 'failure'})
 
     return jsonify({'result': 'success', 'rest_list': dumps(restslist)})
+##########################################################
 
 
+# 글 작성 상세보기 관련
+##########################################################
+# @app.route('/contents')
+# def contents():
+#     return render_template('index.html')
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
+@app.route('/mydetail')
+def my_detail():
+    return render_template('mydetail.html')
+
+@app.route('/mydetail_modifying')
+def modifying_detail():
+    return render_template('mydetail_modifying.html')
+
+@app.route('/otherdetail')
+def other_detail():
+   my_review=db.review.find_one({'user_id': 'test'})
+   likes=my_review['like']
+   return render_template('otherdetail.html',likes=likes)
+
+
+# 맛집 리뷰 POST
+@app.route('/post/mydetail', methods=['POST'])
+def post_my_detail():
+   # 1. 클라이언트로부터 데이터를 받기
+    restaurant_receive=request.form['restaurant_give']
+    category_receive=request.form['category_give']
+    comment_receive=request.form['comment_give']
+   #  이미지 받기
+
+    # 예외 처리
+    if restaurant_receive=="" or comment_receive=="":
+       return jsonify({'result':'empty'})
+
+    # 2. document 만들기 (TEST)
+    review = {'restaurant': restaurant_receive, 'category': category_receive, 'comment': comment_receive, 'like':0, 'locate':'kyeongki', 'name':'jungler','rate':5,'user_id':'test', 'favorite':0 }
+   # 카테고리와 이미지 추가 필요
+
+    # 3. mongoDB에 데이터 넣기
+    db.review.insert_one(review)
+
+    return jsonify({'result':'success'})
+
+@app.route('/modify/mydetail',methods=['POST'])
+def modify_my_detail():
+    # 1. 클라이언트로부터 데이터를 받기
+    restaurant_receive=request.form['restaurant_give']
+    category_receive=request.form['category_give']
+    comment_receive=request.form['comment_give']
+   #  이미지 받기
+
+    # 예외 처리
+    if restaurant_receive=="" or comment_receive=="":
+       return jsonify({'result':'empty'})
+
+    # 2. document 만들기 (TEST)
+    review = {'restaurant': restaurant_receive, 'category': category_receive, 'comment': comment_receive, 'like':0, 'locate':'kyeongki', 'name':'jungler','rate':5,'user_id':'test', 'favorite':0 }
+   # 카테고리와 이미지 추가 필요
+
+    # 3. mongoDB에 데이터 넣기
+    db.review.insert_one(review)
+
+    return jsonify({'result':'success'})
+
+@app.route('/like',methods=['POST'])
+def like_review():
+    # id_receive = request.form['id']
+    # review = db.review.find_one({'_id': ObjectId(id_receive)})
+    review=db.review.find_one({'user_id':'test'})
+    new_likes = review['like'] + 1
+    result = db.review.update_one({'user_id':'test'}, {'$set': {'like': new_likes}})
+    print(review['like'])
+    # 4. 하나의 메모만 영향을 받아야 하므로 result.updated_count 가 1이면  result = success 를 보냄
+    if result.modified_count == 1:
+       return jsonify({'result': 'success'})
+    else:
+       return jsonify({'result': 'failure'})
+
+@app.route('/favorite',methods=['POST'])
+def favorite_review():
+    # id_receive = request.form['id']
+    review=db.review.find_one({'user_id':'test'})
+    print(review)
+   
+    # 이미 즐겨찾기 되어있을 때
+    if review['favorite']==0:
+       result = db.review.update_one({'user_id':'test'}, {'$set': {'favorite':1 }})
+    elif review['favorite']==1:
+       return jsonify({'result':'existed'})
+
+    if result.modified_count == 1:
+      return jsonify({'result': 'success'})
+    else:
+      return jsonify({'result': 'failure'})
+    
+@app.route('/delete',methods=['POST'])
+def delete_review():
+    # id_receive = request.form['id']
+    # result = db.review.delete_one({'_id': ObjectId(id_receive)})
+
+    result = db.review.delete_one({'user_id':'123abc'})
+    # 3. 하나의 영화만 영향을 받아야 하므로 result.updated_count 가 1이면  result = success 를 보냄
+    if result.deleted_count == 1:
+        return jsonify({'result': 'success'})
+    else:
+        return jsonify({'result': 'failure'})
+    
+##########################################################
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
